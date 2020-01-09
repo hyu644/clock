4d56f1f586247229da038fca055e7d1d430d38d1
ikko <ikko@studio-ogawa.com>
2020-01-09 13:51:36 +0900

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by cmStudent on 2020/01/09.
+//  Copyright © 2020 cmStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
